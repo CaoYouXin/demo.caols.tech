4d5ca02000b82cc019dc33b207f6f8648df137f5
曹力升 <c_youxin@163.com>
2016-12-20 16:31:50 +0800

    turns out: bs-config for open custom browser, fixed the bug in index.html

--- hive/angular2/bs-config.js
+++ hive/angular2/bs-config.js
@@ -1,16 +1,13 @@
-module.exports = function(bs) {
+module.exports = function (bs) {
 
     return {
         server: {
             middleware: {
-                // overrides the second middleware default with new settings
-                1: require('connect-history-api-fallback')({
-                    index: '/',
-                    verbose: true
-                })
+                1: require('connect-history-api-fallback')()
             }
         },
-        "browser" : ["google-chrome", "firefox"]
+        browser: ["chrome", "firefox"],
+        // startPath: "./"
     };
 
 };
