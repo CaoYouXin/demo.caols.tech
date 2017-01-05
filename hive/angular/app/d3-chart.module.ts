d3ac2599fe761bad464f5f34341a8f2b22ba40bc
曹力升 <c_youxin@163.com>
2017-01-05 16:14:26 +0800

    dist only

--- hive/angular/app/d3-chart.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from "@angular/core";
-import {D3ChartDirective} from "./d3-chart.directive";
-
-@NgModule({
-    declarations: [D3ChartDirective],
-    exports: [D3ChartDirective]
-})
-export class D3ChartModule {
-}
